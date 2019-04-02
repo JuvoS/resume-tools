f99e38d3f0c0e236b6bc04f7b0b8e85c298a78c5
JuvoS <www.704921698@qq.com>
2019-04-03 00:30:39 +0800

    init resume tools

---
+++ src/components/info/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>infomation input</h2>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import iView from "iview";
+import "iview/dist/styles/iview.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(iView);
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start();
+  next();
+});
+
+router.afterEach(() => {
+  iView.LoadingBar.finish();
+});
 new Vue({
   router,
   store,
